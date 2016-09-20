cba9c8d2ab1e63bb4a75919fbfbe2b5b163a30e3
electricSemyon <electricSemyon@mail.ru>
2016-09-20 14:44:09 +0300

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const core = require('./core');
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.get('/getIntersections', function (req, res) {
   console.log('Ну ёб твою мать, ещё один. Обрабатываю...')
