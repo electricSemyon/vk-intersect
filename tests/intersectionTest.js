9c53cefa9efdf4d1caed6a54692bce7091a33cbc
electricSemyon <electricSemyon@mail.ru>
2016-09-27 20:57:48 +0300

    Fixed tests. Added corrected file endings

--- tests/intersectionTest.js
+++ tests/intersectionTest.js
@@ -2,11 +2,13 @@
 const assert = require('assert');
 const intersect = require('../intersect')
 
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [6, 5, 8, 7, 1, 9, 4];
+describe('#intersect()', function() {
+    it('returns intersections between arrays', function() {
+        const arr1 = [1, 2, 3, 4, 5];
+        const arr2 = [6, 5, 8, 7, 1, 9, 4];
 
-describe('Array intersection', function() {
-    it('Should work...', function() {
-      assert.deepEqual([5, 1, 4], intersect([arr1, arr2]));
+        const intersection = intersect([arr1, arr2]);
+
+        assert.deepEqual([5, 1, 4], intersection);
     });
 });
